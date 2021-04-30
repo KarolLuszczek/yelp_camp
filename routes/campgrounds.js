4bccc85ef62914f933b81d9313ac6935401c1b59
KarolLuszczek <lus.karol@gmail.com>
2021-05-01 00:56:51 +0200

    Star rating

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,24 +5,28 @@ const campgrounds = require('../controllers/campgrounds')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 
-// Basic routes for the router
-// campground/index
-router.get('/', catchAsync(campgrounds.index));
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+
 // campground CREATE route\
 // this route must be declared before /campgroudns/:id
 // otherwise new will be treated as :id 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 // post route to save a new campground
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-// campground SHOW route
-router.get('/:id', catchAsync(campgrounds.showCampground));
-// campground EDIT route
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-// route handling the from request to edit the camp
-// middleware is passed before the main function on the path
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-// camp DELETE route
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCampground));
+router.route('/:id')
+    // campground SHOW route
+    .get(catchAsync(campgrounds.showCampground))
+    // route handling the from request to edit the camp
+    // middleware is passed before the main function on the path
+    .put( isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    // camp DELETE route
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCampground))
+
+    // campground EDIT route
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,14 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.registerUser));
 
-router.post('/register', catchAsync(users.registerUser));
-
-router.get('/login', users.renderLogin);
-
-// Uses passport.authenticate middlewate with local strategy
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect:'/login'}), users.Login);
+router.route('/login')
+    .get(users.renderLogin)
+    // Uses passport.authenticate middlewate with local strategy
+    .post(passport.authenticate('local', {failureFlash: true, failureRedirect:'/login'}), users.Login);
 
 router.get('/logout', users.Logout);
 
